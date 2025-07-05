f77b3d0101a0fc3d8cf4127ae42d1382cc501231
frnwot <frnbuid2005@gmail.com>
2025-07-05 06:55:10 +0000

    Add vendorsetup.sh with improved repo setup and update logic
    Signed-off-by: frnwot <frnbuid2005@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,12 +1,38 @@
+#!/bin/bash
+
+# ===== Colors =====
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+CYAN="\033[1;36m"
+RESET="\033[0m"
+
+log() { echo -e "${CYAN}[INFO]${RESET} $1"; }
+done_msg() { echo -e "${GREEN}[DONE]${RESET} $1"; }
+
+clone_or_update() {
+    local dir=$1
+    local repo=$2
+    local branch=$3
+
+    if [ -d "$dir/.git" ]; then
+        log "Updating existing repo in $dir ..."
+        git -C "$dir" remote set-url origin "$repo"
+        git -C "$dir" fetch --depth=1 origin "$branch"
+        git -C "$dir" reset --hard origin/"$branch"
+        done_msg "Updated $dir"
+    else
+        log "Cloning $repo into $dir ..."
+        git clone --depth=1 -b "$branch" "$repo" "$dir"
+        done_msg "Cloned into $dir"
+    fi
+}
+
 # Vendor Tree
-rm -rf vendor/xiaomi
-git clone --depth=1 -b 14 https://github.com/msm8953F/vendor_xiaomi_daisy vendor/xiaomi/daisy
-git clone --depth=1 -b 14 https://github.com/msm8953F/vendor_xiaomi_msm8953-common vendor/xiaomi/msm8953-common
+clone_or_update vendor/xiaomi/daisy https://github.com/msm8953F/vendor_xiaomi_daisy 14
+clone_or_update vendor/xiaomi/msm8953-common https://github.com/msm8953F/vendor_xiaomi_msm8953-common 14
 
 # Kernel Tree
-rm -rf kernel/xiaomi
-git clone --depth=1 -b 14 https://github.com/msm8953F/kernel_xiaomi_msm8953 kernel/xiaomi/msm8953
+clone_or_update kernel/xiaomi/msm8953 https://github.com/msm8953F/kernel_xiaomi_msm8953 14
 
-# HALS
-rm -rf hardware/xiaomi
-git clone --depth=1 -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+# HALs
+clone_or_update hardware/xiaomi https://github.com/LineageOS/android_hardware_xiaomi lineage-21
